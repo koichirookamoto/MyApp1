57fa80335ed890dc8614f364df9056d011f059eb
Koichiro Okamoto <koichirookamoto@icloud.com>
2025-03-29 12:11:07 +0900

    Initial Commit

---
+++ MyApp1/MyApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  MyApp1App.swift
+//  MyApp1
+//
+//  Created by Okamoto Koichiro on 2025/03/29.
+//
+
+import SwiftUI
+
+@main
+struct MyApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyApp1Tests/MyApp1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MyApp1Tests.swift
+//  MyApp1Tests
+//
+//  Created by Okamoto Koichiro on 2025/03/29.
+//
+
+import Testing
+@testable import MyApp1
+
+struct MyApp1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
